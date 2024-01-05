a4d25ca14194bc630f7757afb35524d45f06b5f2
Shashank krishu <shashankkrishu99@gmail.com>
2024-01-05 22:53:32 +0530

    product models and product apis refacatored according to the new dataset

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -13,75 +13,46 @@ const getAllProducts = async (req, res) => {
 // creating a new product
 const createProduct = async (req, res) => {
   const {
-    product,
-    rating,
-    flavour,
-    roast,
-    brew_method,
-    strong,
-    additional_flavour,
+    Product,
+    Cupping_Score,
+    Sensory,
+    Roast,
+    Brew_Method,
     price,
-    imageUrl,
-    quantity,
-    productInfo
+    Image,
+    Quantity,
+    Product_Info
   } = req.body;
-
   if (
-    !product ||
-    !rating ||
-    !flavour ||
-    !roast ||
-    !brew_method ||
-    !strong ||
-    !additional_flavour ||
+    !Product ||
+    !Cupping_Score ||
+    !Sensory ||
+    !Roast ||
+    !Brew_Method ||
     !price ||
-    !imageUrl ||
-    !quantity ||
-    !productInfo
+    !Image ||
+    !Quantity ||
+    !Product_Info
   ) {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
-
-  const productExists = await ProductSchema.findOne({ product });
-
-  if (productExists) {
-    return res.status(400).json({ msg: "Product already exists" });
-  }
-
-  const newProduct = await ProductSchema.create({
-    product,
-    rating,
-    flavour,
-    roast,
-    brew_method,
-    strong,
-    additional_flavour,
-    price,
-    imageUrl,
-    quantity,
-    productInfo
-  });
-
-  if (newProduct) {
-    res.status(201).json({
-      _id: newProduct._id,
-      product: newProduct.product,
-      rating: newProduct.rating,
-      flavour: newProduct.flavour,
-      roast: newProduct.roast,
-      brew_method: newProduct.brew_method,
-      strong: newProduct.strong,
-      additional_flavour: newProduct.additional_flavour,
-      price: newProduct.price,
-      imageUrl: newProduct.imageUrl,
-      quantity: newProduct.quantity,
-      productInfo: newProduct.productInfo,
-      createdAt: newProduct.createdAt,
-      updatedAt: newProduct.updatedAt
+  try {
+    const product = await ProductSchema.create({
+      Product,
+      Cupping_Score,
+      Sensory,
+      Roast,
+      Brew_Method,
+      price,
+      Image,
+      Quantity,
+      Product_Info
     });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error" });
   }
 };
-
 // getting a product by name
 const getProductByName = async (req, res) => {
   const { prod } = req.params;
@@ -89,24 +60,13 @@ const getProductByName = async (req, res) => {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
 
-  const result = await ProductSchema.findOne({ product: prod });
+  const result = await ProductSchema.findOne({ Product: prod });
   if (result) {
     res.status(200).json({
-      _id: result._id,
-      product: result.product,
-      rating: result.rating,
-      flavour: result.flavour,
-      roast: result.roast,
-      brew_method: result.brew_method,
-      strong: result.strong,
-      additional_flavour: result.additional_flavour,
-      price: result.price,
-      imageUrl: result.imageUrl,
-      quantity: result.quantity,
-      productInfo: result.productInfo
+      result
     });
   } else {
-    res.send(404).json({msg:"Product not found"});
+    res.send(404).json({ msg: "Product not found" });
   }
 };
 
@@ -132,10 +92,9 @@ const ProductSearch = async (req, res) => {
   }
 };
 
-
 module.exports = {
   getAllProducts,
   createProduct,
   getProductByName,
-  ProductSearch,
+  ProductSearch
 };
